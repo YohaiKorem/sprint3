f0f1bfe26d30f8ad51f74eade99af5fb95c86c9e
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-01 11:59:03 +0200

    added minor styling to email preview

--- apps/mail/cmps/MailPreview.js
+++ apps/mail/cmps/MailPreview.js
@@ -2,9 +2,12 @@ export default {
   name: 'MailPreview',
   props: ['mail'],
   template: `
-        <article class="car-preview">
-            <h2>{{ mail.from }}</h2>
-            <h3>{{ mail.to }}</h3>
+        <article class="mail-preview">
+        <span class="mail-labels">{{mail.labels}}</span>  
+         <h2 class="mail-from">from: {{ mail.from }}</h2> <span class="mail-timestamp">sent at: {{mail.sentAt}}</span>
+            <h3 class="mail-to">to: {{ mail.to }}</h3>
+            <p class="mail-cotent">{{mail.body}}</p>
+
         </article>
     `,
 }
