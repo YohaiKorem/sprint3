c26afcb3ca20daf27909ffd985aed2788b3a5395
Ori Krispel <orikrispel8@gmail.com>
2023-03-01 17:55:52 +0200

    NoteEdit added

--- apps/keep/cmps/NotePreview.js
+++ apps/keep/cmps/NotePreview.js
@@ -4,13 +4,15 @@ export default {
   name: 'NotePreview',
   props: ['note'],
   template: `
+    <RouterLink :to="'/keep/edit/' + note.id">
       <article class="note-preview" :style="styleNote">
         <component :is="note.type" :info="note.info" />
         <div class="tool-box flex justify-center">
-          <div class="btn-bg-color" @click="choose-color"></div>
-          <div class="btn-remove" @click="remove"></div>
+          <div class="btn-bg-color" @click.prevent="choose-color"></div>
+          <div class="btn-remove" @click.prevent="remove"></div>
         </div>
       </article>
+    </RouterLink>
   `,
 
   computed: {
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
 // KEEP
 import NoteIndex from './apps/keep/pages/NoteIndex.js'
-import NoteDetails from './apps/keep/pages/NoteDetails.js'
+import NoteEdit from './apps/keep/cmps/NoteEdit.js'
 
 // MAIL
 import MailIndex from './apps/mail/pages/MailIndex.js'
@@ -14,47 +14,57 @@ import SpamPreview from './apps/mail/cmps/SpamPreview.js'
 const { createRouter, createWebHashHistory } = VueRouter
 
 const routerOptions = {
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      component: HomePage,
-    },
-    {
-      path: '/about',
-      component: AboutUs,
-    },
-    // KEEP PATHS
-    {
-      path: '/keep',
-      component: NoteIndex,
-    },
-    {
-      path: '/keep/:noteId',
-      component: NoteDetails,
-    },
-    // MAIL PATHS
-    {
-      path: '/mail',
-      component: MailIndex,
-    },
-    {
-      path: '/mail/:mailId',
-      component: MailEdit,
-    },
-    {
-      path: '/mail/edit/:mailId?',
-      component: MailEdit,
-    },
-    {
-      path: '/mail/trash',
-      component: TrashPreview,
-    },
-    {
-      path: '/mail/spam',
-      component: SpamPreview,
-    },
-  ],
+   history: createWebHashHistory(),
+   routes: [
+      {
+         path: '/',
+         component: HomePage,
+      },
+      {
+         path: '/about',
+         component: AboutUs,
+      },
+
+      // KEEP PATHS
+      {
+         path: '/keep',
+         component: NoteIndex,
+         name: 'keep',
+         children: [{
+            path: '/keep/edit/:id',
+            component: NoteEdit,
+            props: true,
+            name: 'note-edit'
+         }]
+      },
+      // {
+      //    path: '/keep/trash',
+      //    component: keepTrash,
+      //    name: 'keep-trash'
+      // },
+
+      // MAIL PATHS
+      {
+         path: '/mail',
+         component: MailIndex,
+      },
+      {
+         path: '/mail/:mailId',
+         component: MailEdit,
+      },
+      {
+         path: '/mail/edit/:mailId?',
+         component: MailEdit,
+      },
+      {
+         path: '/mail/trash',
+         component: TrashPreview,
+      },
+      {
+         path: '/mail/spam',
+         component: SpamPreview,
+      },
+   ],
 }
 
 export const router = createRouter(routerOptions)
