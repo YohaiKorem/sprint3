4f9fd2c9c1ffe411f4974701cc1432174a1777e7
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-01 20:49:45 +0200

    added styling to MailList

--- apps/mail/cmps/MailPreview.js
+++ apps/mail/cmps/MailPreview.js
@@ -5,12 +5,12 @@ export default {
   props: ['mail'],
   template: `
   <div class="btns-container">
-<button>star</button>
+<button>★</button>
     <button @click="markAsRead">Mark as {{(isRead === 'read') ? 'unread' : 'read'}}</button>
-    <button @click="mail.folder='trash'">Move to trash</button>
+    <button @click="mail.folder='trash'" class="btn-remove"> <img src="assets/img/mailImg/icons/trash.png" class="icon trash-icon"> </button>
   </div>
   <RouterLink class="mail-preview" :to="'mail/' + mail.id">
-        <article :class="isRead">
+        <article :class="isRead + 'mail-preview'">
         <!-- <span class="mail-labels">{{mail.labels}}</span>   -->
          <h2 class="mail-from">from: {{ mail.from }}</h2> <span class="mail-timestamp">sent at: {{mail.sentAt}}</span>
             <!-- <h3 class="mail-to">to: {{ mail.to }}</h3> -->
