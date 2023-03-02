da881298d8ad8bbf4ebb8666dd3c01172bb19f36
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-02 23:29:13 +0200

    added icons to folder list

--- apps/mail/cmps/MailFolderList.js
+++ apps/mail/cmps/MailFolderList.js
@@ -6,13 +6,14 @@ export default {
   template: `
   <h1>{{folder}}</h1>
       <ul class="clean-list folder-list">
-        <li @click="setFolder('inbox')">Inbox</li> <span>{{unreadMailsCount}}</span>
-        <li @click="setFolder('trash')">Trash</li> 
-        <li @click="setFolder('sent')">Sent</li> 
-        <li @click="setFolder('important')">Important</li> 
-        <li @click="setFolder('spam')">Spam</li>
-        <li @click="setFolder('drafts')">Drafts</li>
-        <li @click="setFolder('starred')">Starred</li>
+        <li @click="setFolder('inbox')">Inbox<img class="icon" src="assets/img/mailImg/icons/inbox.png"></li> 
+        <span>{{unreadMailsCount}}</span>
+        <li @click="setFolder('trash')">Trash<img class="icon" src="assets/img/mailImg/icons/trash.png"></li> 
+        <li @click="setFolder('sent')">Sent<img class="icon" src="assets/img/mailImg/icons/sent.png"></li> 
+        <li @click="setFolder('important')">Important<img class="icon" src="assets/img/mailImg/icons/important.png"></li> 
+        <li @click="setFolder('spam')">Spam<img class="spam-icon" src="assets/img/mailImg/icons/spam.png" alt=""> </li>
+        <li @click="setFolder('drafts')">Drafts<img class="icon" src="assets/img/mailImg/icons/draft.png"></li>
+        <li @click="setFolder('starred')">Starred<img class="icon" src="assets/img/mailImg/icons/star.png"> </li>
       </ul>
     `,
   data() {
