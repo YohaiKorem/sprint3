ba5cfb3229a60562ad5fe5f6a72bc622003f9238
Ori Krispel <orikrispel8@gmail.com>
2023-03-02 15:16:49 +0200

    addNote upgrade

--- apps/keep/cmps/AddNote.js
+++ apps/keep/cmps/AddNote.js
@@ -4,25 +4,37 @@ export default {
   name: 'AddNote',
   template: `
       <article class="add-note-container">
-        <form @submit.prevent="save">
-          <input type="text" v-model="note.info.title" placeholder="Title" />
-          <input type="text" v-model="note.info.txt" placeholder="Take a note..." />
-          <button>save</button>
+        <form @submit.prevent="save" @blur="onBlur">
+          <input type="text" v-model="note.info.title" placeholder="Title" v-show="showTitleInput"/>
+          <input type="text" v-model="note.info.txt" placeholder="Take a note..." @focus="onFocus" />
         </form>
+        <div class="types-wrapper flex justify-evenly">
+          <img class="item btn-text" src="../../../assets/img/keep/text.svg" />
+          <img class="item btn-img" src="../../../assets/img/keep/image.svg" />
+          <img class="item btn-todo" src="../../../assets/img/keep/todo.svg" />
+        </div>
       </article>
   `,
   data() {
     return {
       type: 'NoteTxt',
-      note: noteService.getEmptyNote(this.type, undefined),
+      note: noteService.getEmptyNote(this.type),
+      showTitleInput: false,
     }
   },
+
   methods: {
     save() {
-      this.note = noteService.getEmptyNote()
+      this.note = noteService.getEmptyNote(this.note.info, this.type)
       this.$emit('saveNote', this.note)
       this.note = noteService.getEmptyNote()
-    }
-  },
-  emits: ['saveNote'],
+    },
+    onFocus() {
+      this.showTitleInput = true
+    },
+    onBlur() {
+      this.showTitleInput = false
+    },
+    emits: ['saveNote'],
+  }
 }
