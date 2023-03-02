f248f24a6bebc5944455fce17d5f67fba2472512
Ori Krispel <orikrispel8@gmail.com>
2023-03-02 14:12:12 +0200

    NoteImg added

---
+++ apps/keep/cmps/NoteTools.js
@@ -0,0 +1,64 @@
+import { noteService } from '../services/note.service.js'
+import { eventBusService } from '../../../services/event-bus.service.js'
+import ColorPicker from './ColorPicker.js'
+
+export default {
+  name: 'NoteTools',
+  props: ['note'],
+  template: `
+        <article class="tool-box flex justify-center">
+          <div class="item btn-bg-color" title="background color" @click.stop.prevent="toggleColorPicker"></div>
+          <ColorPicker ref="colorPicker" :note="note" @updateColor="updateColor" v-show="showColorPicker"/>
+          <label class="item btn-upload-img" for="image" title="upload image" @click.stop>
+            <input type="file" class="file-input btn" accept="image/png, image/jpeg"  name="image" id="image" @change="getImgUrl" />
+          </label>
+          <div class="item btn-remove" title="remove" @click.prevent="remove"></div>
+        </article>
+  `,
+
+  data() {
+    return {
+      showColorPicker: false,
+    }
+  },
+  methods: {
+
+    remove() {
+      this.$emit('remove', this.note.id)
+    },
+
+    updateColor(color) {
+      this.note.style.backgroundColor = color
+      eventBusService.emit('updateNote', this.note)
+    },
+
+    toggleColorPicker() {
+      this.showColorPicker = !this.showColorPicker
+    },
+
+    closeColorPicker(event) {
+      if (this.showColorPicker && !this.$refs.colorPicker.$el.contains(event.target)) {
+        this.showColorPicker = false
+      }
+    },
+
+    getImgUrl(ev) {
+      const url = noteService.createImg(ev)
+        .then(url => this.$emit('updateImgUrl', url))
+    },
+  },
+
+  mounted() {
+    document.addEventListener('click', this.closeColorPicker)
+  },
+
+  beforeUnmount() {
+    document.removeEventListener('click', this.closeColorPicker)
+  },
+
+  components: {
+    ColorPicker,
+  },
+
+  emits: ['updateImgUrl', 'remove'],
+}
