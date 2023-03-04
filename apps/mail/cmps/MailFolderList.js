be0d347910ae5bc4d356b780baaa9d1861124100
YohaiKorem <Yohaycasoy@gmail.com>
2023-03-04 11:41:13 +0200

    added styling and reposnive nav bar to mobile

--- apps/mail/cmps/MailFolderList.js
+++ apps/mail/cmps/MailFolderList.js
@@ -9,41 +9,57 @@ export default {
                 <div class="bar"></div>
                 <div class="bar"></div>
             </button>
-  <aside class="mail-folder-list">
-    <ul class="clean-list folder-list">
-      <li @click="setFolder('inbox')"><div class="inside-folder-item-container">
-        <img class="icon" src="assets/img/mailImg/icons/inbox.png">Inbox</div> </li> 
-      <span>{{unreadMailsCount}}</span>
-      <li @click="setFolder('trash')"><div class="inside-folder-item-container">
-        <img class="icon" src="assets/img/mailImg/icons/trash.png">Trash</div> </li> 
-      <li @click="setFolder('sent')"><div class="inside-folder-item-container">
-        <img class="icon sent-icon" 
-        src="assets/img/mailImg/icons/sent.png">Sent
-      </div>
-    </li>
-<li @click="setFolder('archive')">
-<div class="inside-folder-item-container"><img class="icon" src="assets/img/mailImg/icons/archive.svg">Archive
-  </div>
- </li> 
-      <li @click="setFolder('important')">
-        <div class="inside-folder-item-container">
-<div class="important-icon-container">
+ <aside :class="isMenuOpen ? 'open ' + 'mail-folder-list' : 'mail-folder-list'">
+   <ul class="clean-list folder-list">
+        <li @click="setFolder('inbox')">
+           <div class="inside-folder-item-container">
+              <img class="icon" src="assets/img/mailImg/icons/inbox.png"> Inbox
+          </div> 
 
-  <svg class="important-icon icon"  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path class="important-icon" fill-rule="evenodd" clip-rule="evenodd" d="M15.5 5C16.17 5 16.77 5.33 17.13 5.84L21.5 12L17.13 18.16C16.77 18.67 16.17 19 15.5 19L4.5 18.99C3.4 18.99 2.5 18.1 2.5 17V7C2.5 5.9 3.4 5.01 4.5 5.01L15.5 5ZM4.5 17H15.5L19.05 12L15.5 7H4.5V17Z" fill="black"/>
-  </svg>
-</div>          
-Important
-          <!-- <img class="icon" src="assets/img/mailImg/icons/important.svg"> -->
+          <span class="unread-mails-count">{{unreadMailsCount}}</span>
+       </li> 
+
+       <li @click="setFolder('trash')">
+          <div class="inside-folder-item-container">
+             <img class="icon" src="assets/img/mailImg/icons/trash.png"> Trash
+           </div> 
+     </li> 
+      <li @click="setFolder('sent')">
+         <div class="inside-folder-item-container">
+           <img class="icon sent-icon" src="assets/img/mailImg/icons/sent.png"> Sent
+       </div>
+    </li>
+        <li @click="setFolder('archive')">
+          <div class="inside-folder-item-container">
+            <img class="icon" src="assets/img/mailImg/icons/archive.svg">Archive
+          </div>
+        </li> 
+        <li @click="setFolder('important')">
+           <div class="inside-folder-item-container">
+            <div class="important-icon-container">
+               <svg class="important-icon icon"  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path class="important-icon" fill-rule="evenodd" clip-rule="evenodd" d="M15.5 5C16.17 5 16.77 5.33 17.13 5.84L21.5 12L17.13 18.16C16.77 18.67 16.17 19 15.5 19L4.5 18.99C3.4 18.99 2.5 18.1 2.5 17V7C2.5 5.9 3.4 5.01 4.5 5.01L15.5 5ZM4.5 17H15.5L19.05 12L15.5 7H4.5V17Z" fill="black"/>
+               </svg>
+            </div>Important
         </div>
       </li> 
-      <li @click="setFolder('spam')"><div class="inside-folder-item-container">
-        <img class="spam-icon" src="assets/img/mailImg/icons/spam.svg" alt="">Spam</div> </li>
-      <li @click="setFolder('drafts')"><div class="inside-folder-item-container">
-        <img class="icon" src="assets/img/mailImg/icons/draft.png">Drafts</div></li>
-      <li @click="setFolder('starred')"><div class="inside-folder-item-container">
-         <div class="star-inside-folder-list">★</div>Starred</div> </li>
-    </ul>
+      <li @click="setFolder('spam')">
+        <div class="inside-folder-item-container">
+             <img class="spam-icon" src="assets/img/mailImg/icons/spam.svg" alt="">Spam
+      </div>
+     </li>
+      <li @click="setFolder('drafts')">
+           <div class="inside-folder-item-container">
+            <img class="icon" src="assets/img/mailImg/icons/draft.png">Drafts
+           </div>
+    </li>
+    <li @click="setFolder('starred')">
+      <div class="inside-folder-item-container">
+           <div class="star-inside-folder-list">
+              ★
+            </div>Starred
+      </div>
+   </li>
+</ul>
   </aside>
     `,
   data() {
@@ -53,20 +69,26 @@ Important
       isMenuOpen: false,
     }
   },
-  created() {},
+  created() {
+    eventBusService.on('toggleMenu', (isMenuOpen) => {
+      this.isMenuOpen = isMenuOpen
+    })
+    eventBusService.on('changeReadCount', (unreadMailsCount) => {
+      this.unreadMailsCount = unreadMailsCount
+    })
+  },
   methods: {
     setFolder(folder) {
       this.folder = folder
       this.$emit('setFolder', folder)
+      this.isMenuOpen = false
+      eventBusService.emit('toggleMenu', this.isMenuOpen)
     },
-  },
-  methods: {
+
     openMenu() {
       this.isMenuOpen = !this.isMenuOpen
       eventBusService.emit('toggleMenu', this.isMenuOpen)
     },
   },
-  // created() {
-  //   this.setFolder('inbox')
-  // },
+  emits: ['toggleMenu', 'setFolder'],
 }
--- apps/mail/pages/MailIndex.js
+++ apps/mail/pages/MailIndex.js
@@ -18,22 +18,24 @@ export default {
    <img class="icon pencil-icon" src="assets/img/mailImg/icons/pencil.png">
    Compose</button>  
      </RouterLink>
-
-  <!-- <div class="input-heading-container full">
+     
+     <!-- <div class="input-heading-container full">
 <div class="btns-container">
   <h1>Your inbox</h1>
   <button @click="test">test</button>
   <button @click="clearStorage">clear</button>
 </div> -->
 
+<section v-if="!hasMails"  class="no-mails-modal">There are no mails in the {{folder}} folder</section>
 
 
-</div>
+
+<!-- </div> -->
   <MailFolderList @setFolder="setFolder"/>
     <MailFilter  @filter="setCriteria"/>
     <!-- <button @click="removeForEver" class="btn-delete-forever">delete forever</button> -->
     <MailList :mails="filteredMails"/> 
-        <div @click="toggleMenu" v-if="isMenuOpen" class="modal-overlay"></div>
+        <div @click="toggleMenu" v-if="isMenuOpen" class="modal-backdrop"></div>
       </section>
       <router-view />
 `,
@@ -44,19 +46,15 @@ export default {
       folder: 'inbox',
       unreadMailsCount: 0,
       isMenuOpen: false,
+      hasMails: null,
     }
   },
   created() {
-    mailService.query().then((mails) => {
-      this.mails = mails
-      mails.forEach((mail) => {
-        if (!mail.isRead) this.unreadMailsCount++
+    this.render(),
+      eventBusService.on('update', (mail) => {
+        // console.log(this.folder)
+        mailService.save(mail).then(() => this.render())
       })
-    })
-    eventBusService.on('update', (mail) => {
-      // console.log(this.folder)
-      mailService.save(mail).then(() => this.render())
-    })
     eventBusService.on('toggleMenu', (isMenuOpen) => {
       this.isMenuOpen = isMenuOpen
     })
@@ -97,9 +95,22 @@ export default {
       this.criteria = criteria
     },
     render() {
-      mailService.query(this.folder).then((mails) => (this.mails = mails))
+      mailService.query(this.folder).then((mails) => {
+        !mails.length ? (this.hasMails = null) : (this.hasMails = true)
+        let count = 0
+        mails.forEach((mail) => {
+          if (!mail.isRead) count++
+        })
+
+        this.unreadMailsCount = count
+        eventBusService.emit('changeReadCount', this.unreadMailsCount)
+        this.mails = mails
+      })
     },
-    openMenu() {},
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+      eventBusService.emit('toggleMenu', this.isMenuOpen)
+    },
   },
   computed: {
     filteredMails() {
